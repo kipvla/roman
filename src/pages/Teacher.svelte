0d77760dbb43cb29c5eec32e79d2fb90a54b5f9b
Kip Riecken <kip.riecken@gmail.com>
2021-01-22 13:39:42 -0500

    fix teacher image render

--- src/pages/Teacher.svelte
+++ src/pages/Teacher.svelte
@@ -1,7 +1,21 @@
-<div class="container d-flex mt-5 pt-5">
+<div class="container d-flex mt-5 pt-5 justify-content-center">
     <div class="d-flex flex-column justify-content-center"> 
         <p>I enjoy teaching music as much as I do performing. I teach the Suzuki method with a large variety of additional repertoire including new music, folk music, pop music, and student compositions. I am constantly searching for new works and composers from underrepresented groups to provide students with a broad appreciation for the amazing diversity in classical music and music in general. My students can be found studying diverse pieces from J.S. Bach to Florence Price to Lindsey Stirling.</p>
         <p>Roman currently teaches in Guanajuato, México as well as via Skype. He teaches in English and Spanish.</p>
     </div>
-    <img src="images/roman.jpg" class="w-50" alt="">
+    <img src="images/roman.jpg" alt="">
 </div>
+
+<style>
+    img {
+        max-width: 50%;
+    }
+    @media only screen and (max-width: 768px) {
+        img {
+            max-width: 80%;
+        }
+        .container {
+            flex-wrap: wrap;
+        }
+    }
+</style>
